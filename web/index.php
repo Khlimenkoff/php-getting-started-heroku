19b3791e88eec309f2cab893e77f83edecea620a
Khlimenkoff <36482950+Khlimenkoff@users.noreply.github.com>
2018-02-14 20:28:38 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -10,28 +10,34 @@
   <?php if (isset($_GET['submitted'])) { ?>
   <div style="border: solid red 1px">
     <h1>The server had accepted:</h1>
-    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
-    <p>text: <?php echo htmlspecialchars($_GET['text']); ?></p>
-    <p>text-area: <?php echo htmlspecialchars($_GET['area']); ?></p>
+    <p>Radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
+    <p>Text: <?php echo htmlspecialchars($_GET['text']); ?></p>
+    <p>Text-area: <?php echo htmlspecialchars($_GET['area']); ?></p>
+    <p>E-mail: <?php echo htmlspecialchars($_GET['pismo']); ?></p>
+    <p>Checkbox: <?php echo htmlspecialchars($_GET['galochka']); ?></p>
     <p>Try again? <a href="/index.php">Yes!</a></p>
     </div>
   <?php } ?>
 
   <?php if (!isset($_GET['submitted'])) { ?>
   
-  <div style="background-color: lightblue; border-radius:20px; width: 400px; padding:20%">
+  <div style="background-color: lightblue; border-radius:20px; width: 200px; padding:5%">
   <h1>Play with the HTML form</h1>
   <form action="/index.php">
     <input type="hidden" name="submitted" value="yes">
     <div>
-      <p><label for="my-radio-yes">radio Yes: </label>
+      <p><label for="my-radio-yes">Radio Yes: </label>
       <input id="my-radio-yes" name="radio" type="radio" required value="Yes"> </p>
-      <p><label for="my-radio-no">radio No: </label>
+      <p><label for="my-radio-no">Radio No: </label>
       <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
-      <p><label for="text-field">text field</label>
+      <p><label for="text-field">Text field </label>
       <input id="text-field" name="text" type="text" required></p>
-      <p><label for="text-area">text area</label>
+      <p><label for="text-area">Text area </label>
       <input id="text-area" name="area" type="area" required></p>
+      <p><label for="e-mail">E-mail: </label>
+      <input id="e-mail" name="pismo" type="e-mail" required></p>
+      <p><label for="checkbox">Checkbox: </label>
+      <input id="checkbox" name="galochka" type="checkbox" required></p>
     </div>
     <br>
     <input type="submit" value="Submit">
